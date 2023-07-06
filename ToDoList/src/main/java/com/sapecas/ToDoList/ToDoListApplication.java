286767982089291e1ae43ba3e3b8b2db5fd86046
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2023-07-05 21:44:02 -0300

    TESTE2

--- ToDoList/src/main/java/com/sapecas/ToDoList/ToDoListApplication.java
+++ ToDoList/src/main/java/com/sapecas/ToDoList/ToDoListApplication.java
@@ -9,7 +9,7 @@ public class ToDoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
-                System.out.println("teste");
+                System.out.println("teste2");
 	}
         
         
